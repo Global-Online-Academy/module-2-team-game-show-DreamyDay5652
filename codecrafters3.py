90f1bccecdd6d2b250c5fa896b9cd7cdbfc79767
Egor Naumenko <egornaumenko2007@gmail.com>
2025-02-18 20:23:10 +0200

    included the function that i was the driver for

--- codecrafters3.py
+++ codecrafters3.py
@@ -1,6 +1,7 @@
 # YOU ONLY HAVE TO SUBMIT FUNCTIONS FOR WHICH
 # YOU ARE THE DRIVER IN PAIR PROGRAMMING
 
+import random
 
 # Here are some history variables to test your code on. Feel free to create your own.
 hist1 = []
@@ -20,4 +21,27 @@ hist4 = [("split","steal"),("steal","steal"),("split","steal"),("steal","split")
 
 # Your team's 3rd strategy (leave blank if you are not the driver)
 # Explanation of Strategy:
-# 
+def codeCrafters3(history):
+    opp_hist = [move[1] for move in history]
+
+    if len(opp_hist) < 3:
+        return 'steal'
+    
+    splits = opp_hist.count('split')
+    steals = opp_hist.count('steal')
+    total = len(opp_hist)
+
+    if splits / total > 0.5:
+        return 'steal'
+    
+    if steals / total > 0.5:
+        if random.random() < 0.7:
+            return 'steal'
+        else:
+            return 'split'
+    
+    if random.random() < 0.6:
+        return 'steal'
+    else:
+        return'split'
+
